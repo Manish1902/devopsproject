14394de72a7e6f0ac56b2bfcd239cee41656f933
Manish Singh Talan <manishtalan19@gmail.com>
2024-11-10 17:38:32 +0530

    .

--- src/App.test.js
+++ src/App.test.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders edit src/App.js and save to reload text', () => {
   render(<App />);
-  const linkElement = screen.getByText((content, element) => {
-    return element.tagName.toLowerCase() === 'p' && /edit src\/App.js and save to reload/i.test(content);
-  });
+  const linkElement = screen.getByText(/edit src\/App.js and save to reload/i);
   expect(linkElement).toBeInTheDocument();
 });
