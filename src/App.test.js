606c689c9d6baa472908a8682203c90e4da11fa9
Manish Singh Talan <manishtalan19@gmail.com>
2024-11-10 17:35:57 +0530

    .

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,8 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/edit src\/App.js and save to reload/i);
+  const linkElement = screen.getByText((content, element) => {
+    return element.tagName.toLowerCase() === 'p' && /edit src\/App.js and save to reload/i.test(content);
+  });
   expect(linkElement).toBeInTheDocument();
 });
