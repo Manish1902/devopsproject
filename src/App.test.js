22f3e0ea12dec780752cdd6f65786e43f512a464
Manish Singh Talan <manishtalan19@gmail.com>
2024-11-13 00:40:11 +0530

    .

--- src/App.test.js
+++ src/App.test.js
@@ -10,6 +10,6 @@ test('renders welcome message', () => {
 
 test('renders simple description', () => {
   render(<App />);
-  const descriptionElement = screen.getByText('This is a simple React application made by Manish Singh Talan');
+  const descriptionElement = screen.getByText('This is a simple React application made by Abhishek');
   expect(descriptionElement).toBeInTheDocument();
 });
